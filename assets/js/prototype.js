8f75dcf8db81878775f184775beac035d485b0f2
FredericoAndrade <faandrade@gmail.com>
2021-02-19 11:47:44 -0500

    iterate

--- assets/js/prototype.js
+++ assets/js/prototype.js
@@ -26,63 +26,6 @@ updateSlider()
 // updateTools
 // reportGridSize
 
-class Grid {
-  constructor(columns, rows, border) {
-    this.columns = columns,
-    this.rows = rows,
-    this.border = border,
-    this.origin = 1;
-    this.cellCount = columns * rows;
-    this.columnSet = [];
-    this.rowSet = [];
-    this.cells = [];
-    this.borderColor = "white";
-  };
-  get getCells() {
-    return this.columns * this.rows;
-  };
-};
-
-class Column {
-	constructor(index) {
-		this.index = index
-	}
-}
-
-class Row {
-	constructor(index) {
-		this.index = index
-	}
-}
-
-class Cell {
-  constructor(col,row,index) {
-    // this.datum = obj ? obj : getRandomCell();
-    // this.title = this.datum.title;
-    // this.key = this.datum.id;
-    // this.image = this.datum.img;
-    // this.url = this.datum.youtube
-    this.index = index;
-		this.color = this.getRandomColor();
-    this.column = col;
-    this.row = row;   
-  };
-  get address() {
-  	return this.getAddress();
-  }
-  getAddress() {
-  	return( `c${this.column}r${this.row}`)
-  }
-  getRandomColor() {
-	  const letters = "0123456789ABCDEF";
-	  let color = "#";
-	  for (var i = 0; i < 6; i++) {
-	    color += letters[8 + Math.floor(Math.random() * 8)];
-	  };
-	  return color;
-	};
-};
-
 // const grid = new Grid(7,6,2);
 const grid = new Grid(4,3,2);
 
