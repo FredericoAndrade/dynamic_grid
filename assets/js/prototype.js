f130c7704bc4e00a635738ba8f0d6924fbe5552f
FredericoAndrade <faandrade@gmail.com>
2021-02-19 12:03:51 -0500

    iterate

--- assets/js/prototype.js
+++ assets/js/prototype.js
@@ -358,6 +358,7 @@ function updateTools() {
 }
 
 function reportGridSize() {
+	console.log("hi")
 	console.log("Grid size: ", grid)
 }
 
@@ -370,7 +371,7 @@ $("button#addTop").on("click",function(){addTop()})
 $("button#addBottom").on("click",function(){addBottom()})
 $("button#removeTop").on("click",function(){removeTop()})
 $("button#removeBottom").on("click",function(){removeBottom()})
-$("#gridSize").on("click", function() {reportGridSize()})
+$("#gridSize").on("click", function() {console.log("hi");reportGridSize()})
 
 // $(document).on("mousedown",".cell", function(e) {
 // 	const target = grid.cells.filter(e => e.index == this.dataset.cell)[0]
